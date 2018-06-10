0870641d0f26ff8d2519cc8b263d5739cb3308a0
Kyle Mulqueen <kmulqdrums710@gmail.com>
2018-06-09 22:00:56 -0600

    Last commit. Couldn't get certain capital letters to work so everything is lowercase. Also couldn't get the complete phrase to show upon win or loss.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,18 +4,17 @@ var infoBank = [
      pianoOne = {
          firstName: "Herbie",
          lastName: "Hancock",
-         personalHint: "Known for playing with Miles Davis and leading the Head Hunters."
+         personalHint: "Known for playing with Miles Davis and leading the Head Hunters.",
      },
      pianoTwo = {
          firstName: "Bill",
          lastName: "Evans",
-         personalHint: "Known for leading their own trio for and contributing to the record 'Kind of Blue'."
+         personalHint: "Known for leading his own trio for and contributing to the record 'Kind of Blue', everybody digs this guy."
      },
      pianoThree = {
          firstName: "Ahmad",
          lastName: "Jamal",
          personalHint: "Known for being very expressive with space and phrasing, Pete Rock sampled from this pianists version of 'I Love Music', to produce the beat for 'The World is Yours' by Nas."
-        
      },
      pianoFour = {
          firstName: "Larry",
@@ -25,22 +24,22 @@ var infoBank = [
      bassOne = {
          firstName: "Reggie",
          lastName: "Workman",
-         personalHint: "Bass Player"
+         personalHint: "Bass Player known for playing with Wayne Shorter, John Coltrane, and the Jazz Messengers."
      },
      bassTwo = {
          firstName: "Ron",
          lastName: "Carter",
-         personalHint: "Bass Player"
+         personalHint: "Known more for his work with Miles Davis, this bass player also contributed the bass line for 'Verses From the Abstract' off of the record 'Low End Theory' from A Tribe Called Quest in 1991."
      },
      bassThree = {
          firstName: "Jimmy",
          lastName: "Garrison",
-         personalHint: "Bass Player"
+         personalHint: "Replacing Reggie Workman in John Coltrane's quartet, this lyrical bass player is featured on 'A Love Supreme' as well as many other Coltrane records."
      },
      bassFour = {
          firstName: "Richard",
          lastName: "Davis",
-         personalHint: "Bass Player"
+         personalHint: "This classically trained bass player played with many of the greats including Miles Davis, Sarah Vaughn, Frank Sinatra, Eric Dolphy, Elvin Jones, Ahmad Jamal, and the Thad Jones and Mel Lewis Orchestra."
      },
      drumsOne = {
          firstName: "Elvin",
@@ -55,12 +54,12 @@ var infoBank = [
      drumsThree = {
          firstName: "Billy",
          lastName: "Higgins",
-         personalHint: "This drummer is known for his ability to control a band with his ride cymbal and to not do anything too flashy. He was Lee Morgan's go-to drummer."
+         personalHint: "This drummer is known for being able to control a band with his ride cymbal as well as being very taseful. He was Lee Morgan's go-to drummer. He was also an active part in the avant-garde scene."
      },
      drumsFour = {
          firstName: "Max",
          lastName: "Roach",
-         personalHint: "Known as an extremely melodic drummer."
+         personalHint: "Known as an extremely melodic drummer, he was an integral part of the bebop era. His record 'Drums Unlimited' consists of several drum-only compositions."
      }
 ];
 
@@ -77,13 +76,14 @@ var wins = 0,
 
 
 // Creating a start game function. It randomly generates a new word to solve from the info bank and writes the string value to the document.
+alert("Press Any Key to Start!");
 
 function startGame(){
     //Set up new game
     guessCount = 9;
-    document.getElementById("guesses-remaining").textContent = "Guesses Remaining: " + guessCount;
+    document.getElementById("guesses-remaining").textContent = guessCount;
     wrongGuess = [];
-    document.getElementById("wrong-letters").textContent = "Wrong Letters: " + wrongGuess;
+    document.getElementById("wrong-letters").textContent = wrongGuess;
     userGuess = [];
     underScores = [];
 
@@ -107,46 +107,55 @@ function startGame(){
         document.getElementById("phrase").textContent = underScores.join("");
         document.getElementById("hint").textContent = "Hint: " + hint;
         
-        console.log(underScores);
-        console.log(word);
+        
+        
     };
-
+    console.log(word);
 };
+
 
 // Getting users guess input
 document.onkeyup = function(event) {
-    userGuess = (event.key);
-    console.log(userGuess);
-    var word = newWord.firstName + " " + newWord.lastName;
 
+   if(event.keyCode >= 65 && event.keyCode <=90) {
+        userGuess = (event.key);
+   }
+
+    var word = newWord.firstName.toLowerCase() + " " + newWord.lastName.toLowerCase();
+    // console.log(word);
+    
     // Determine wether the guess exists in the word or not
     if(word.indexOf(userGuess) > -1){
         for(var j = 0; j < word.length; j++){
-            // Letters that DON'T WORK for toLowerCase for some reason: w, b, e, l, j, g, h, c, m
+            // Letters that DON'T WORK for toLowerCase for some reason: w, b, e, l, j, g, h, c, m, and sometimes r.
             if(word[j].toLowerCase() === userGuess.toLowerCase()){
                 underScores[j] = word[j];
                 document.getElementById("phrase").textContent = underScores.join("");
             };
         }
-    } else {
+    } else if (underScores.indexOf(userGuess.toLowerCase()) === -1 && underScores.indexOf(userGuess.toUpperCase() === -1)) {
         wrongGuess.push(userGuess);
-        document.getElementById("wrong-letters").textContent = "Wrong Letters: " + wrongGuess; 
+        document.getElementById("wrong-letters").textContent = wrongGuess; 
         guessCount--;
-        document.getElementById("guesses-remaining").textContent = "Guesses remaining: " + guessCount;
+        document.getElementById("guesses-remaining").textContent = guessCount;
     };
+
     // Ending the game when guessCount = 0 (LOSS)
     if(guessCount === 0) {
         losses++;
-        document.getElementById("losses").textContent = "Losses: " + losses;
-        // document.getElementById("phrase").textContent = word; Trying to get the word to display when the user loses so they can see the answer.
+        document.getElementById("losses").textContent = losses;
+        // Trying to get the word to display when the user loses so they can see the answer. DOESN'T WORK.
+        document.getElementById("phrase").textContent = word; 
         alert("You ran out of guesses!");
         // Restart Game        
         startGame();
         // Ending the game when phrase is correct (WIN)
     } else if(underScores.join("") === word) {
-        alert("You win!");
         wins++;
-        document.getElementById("wins").textContent = "Wins: " + wins;
+        document.getElementById("wins").textContent = wins;
+        // Trying to get the word to display when the user wins so they can see the answer. DOESN'T WORK.
+        document.getElementById("phrase").textContent = word;
+        alert("You win!");
         // Restart Game
         startGame();
     };
@@ -154,6 +163,8 @@ document.onkeyup = function(event) {
 
 
 startGame();
+
+
 
 
 
